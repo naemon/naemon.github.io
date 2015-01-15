6b134059ed09b300bcba67af82241201ba3699a4
Sven Nierlein <sven@nierlein.de>
2015-01-15 10:34:22 +0100

    rework test suite
    make test script project independant so it can be used in other
    projects too. Also hide some more errors.
    
    Signed-off-by: Sven Nierlein <sven@nierlein.de>

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-
+TESTPORT=4001
 
 test:
 	bundle exec jekyll serve & SPID=$$!;  \
 		sleep 10; \
-		./t/test_website.pl http://localhost:4000; \
+		TESTEXPECT=Naemon TESTTARGET=http://localhost:$(TESTPORT) PERL_DL_NONLAZY=1 perl -MExtUtils::Command::MM -e "test_harness(0)" t/*.t; \
 		RC=$$?; \
 		kill -9 $$SPID; \
 		exit $$RC
--- t/test_website.pl
+++ t/test_website.pl
@@ -6,80 +6,58 @@ use Getopt::Long;
 use Test::More;
 use Data::Dumper;
 use Getopt::Long;
+use Cwd;
 use Time::HiRes qw(gettimeofday tv_interval);
 
 #########################################################################
 # parse and check cmd line arguments
-my ($opt_v, $opt_h, $mainurl) = (0);
+my ($opt_v, $opt_h, $mainurl, $opt_expect) = (0, undef, $ENV{'TESTTARGET'}, $ENV{'TESTEXPECT'});
 Getopt::Long::Configure('no_ignore_case');
 Getopt::Long::Configure('bundling');
 GetOptions (
    "h"    => \$opt_h,
    "v"    => sub { $opt_v++; },
+   "e=s"  => \$opt_expect,
    "<>"   => sub { $mainurl = shift; },
 );
 if($opt_h or !$mainurl) {
     print "usage: $0 <url>\n";
     exit 3;
 }
-my $t = TestUtils->new();
-my $output;
-Test::More->builder->output(\$output);
-Test::More->builder->failure_output(\$output);
-Test::More->builder->todo_output(\$output);
-eval {
-    my $test = {
-            url                     => "".$mainurl,
-            like                    => 'Naemon',
-            recursive               => 1,
-            follow                  => 1,
-            check_anchors           => 1,
-            check_external_links    => qr/\.(jpg|png|gif|js|css)$/, # only check images and stylesheets, we do not want to increase our download statistics here
-    };
-    $t->test_page(%{$test});
-
-    my $num_to_check = 1;
-    while($num_to_check > 0) {
-        for my $url (sort keys %{$TestUtils::pages_to_check}) {
-            next if $TestUtils::pages_to_check->{$url} == 1;
-            $test->{'url'} = $url;
-            $t->test_page(%{$test});
-        }
-        $num_to_check = 0;
-        for my $url (sort keys %{$TestUtils::pages_to_check}) {
-            next if $TestUtils::pages_to_check->{$url} == 1;
-            $num_to_check++;
-        }
+if($mainurl =~ m|^file://(.*)$|mx) {
+    my $dir = $1;
+    if(-d $dir) {
+        $dir =~ s|/[^/]*$||gmx;
     }
+    chdir($dir) || die("cannot change to $dir");
+}
+my $t = TestUtils->new();
+my $test = {
+        url                     => "".$mainurl,
+        like                    => $opt_expect,
+        recursive               => 1,
+        follow                  => 1,
+        check_anchors           => 1,
+        check_external_links    => qr/\.(jpg|jpeg|png|gif|js|css|ico)$/i, # only check images and stylesheets, we do not want to increase our download statistics here
 };
-if($@) {
-    print "UNKNOWN - $@\n";
-    exit 3;
-}
-my($ok,$nok) = (0,0);
-my @summary  = Test::More->builder->summary();
-my $tests    = scalar @summary;
-for my $t (@summary) {
-  if($t) {$ok++; } else {$nok++;}
+$t->test_page(%{$test});
+
+my $num_to_check = 1;
+while($num_to_check > 0) {
+    for my $url (sort keys %{$TestUtils::pages_to_check}) {
+        next if $TestUtils::pages_to_check->{$url} == 1;
+        $test->{'url'} = $url;
+        $t->test_page(%{$test});
+    }
+    $num_to_check = 0;
+    for my $url (sort keys %{$TestUtils::pages_to_check}) {
+        next if $TestUtils::pages_to_check->{$url} == 1;
+        $num_to_check++;
+    }
 }
 
-# cleanup output unless verbose
-if($opt_v == 0) {
-    $output =~ s/^ok.*\s//gmx;
-    $output =~ s/^\#\s+Failed\stest.*\s//gmx;
-    $output =~ s/^\#\s+at\s.*\s//gmx;
-}
-
-if($nok > 0) {
-    print "CRITICAL - $nok/$tests tests failed\n";
-    print $output;
-    print "failed $nok/$tests tests\n";
-    exit 2;
-} else {
-    print "OK - $tests tests passed\n";
-    print $output;
-    exit 0;
-}
+done_testing();
+exit;
 
 ###############################################################################
 package TestUtils;
@@ -166,6 +144,13 @@ sub test_page {
         }
     }
 
+    # test the content type
+    $return->{'content_type'} = $request->header('Content-Type');
+    my $content_type = $request->header('Content-Type');
+    if(defined $opts->{'content_type'}) {
+        is($return->{'content_type'}, $opts->{'content_type'}, 'Content-Type should be: '.$opts->{'content_type'}) or diag($opts->{'url'});
+    }
+
     # text that should appear
     if(defined $opts->{'like'}) {
         if(ref $opts->{'like'} eq '') {
@@ -187,14 +172,6 @@ sub test_page {
             }
         }
     }
-
-    # test the content type
-    $return->{'content_type'} = $request->header('Content-Type');
-    my $content_type = $request->header('Content-Type');
-    if(defined $opts->{'content_type'}) {
-        is($return->{'content_type'}, $opts->{'content_type'}, 'Content-Type should be: '.$opts->{'content_type'}) or diag($opts->{'url'});
-    }
-
 
     # html valitidy
     if($content_type =~ 'text\/html' and !$request->is_redirect) {
@@ -220,14 +197,20 @@ sub test_page {
     # check for missing images / css or js
     my $local_check_anchors = {};
     if($content_type =~ 'text\/html') {
-        my @matches1 = $return->{'content'} =~ m/\s+(src|href)='(.+?)'/gi;
-        my @matches2 = $return->{'content'} =~ m/\s+(src|href)="(.+?)"/gi;
-        my @matches3 = $return->{'content'} =~ m/<meta\s+http\-equiv=.*?refresh(.*)URL=([^'"]+)/gi;
+        my $content = $return->{'content'};
+        # remove script tags without a src
+        $content =~ s/<script[^>]*src=('|")([^'"]*)('|")>\s*<\/script>/**SCRIPT:$2**/gsmxio;
+        $content =~ s/<script[^>]*>.+?<\/script>//gsmxio;
+        $content =~ s/\*\*SCRIPT:(.*?)\*\*/<script src="$1"><\/script>/gsmxio;
+        my @matches1 = $content =~ m/\s+(src|href)='(.+?)'/gi;
+        my @matches2 = $content =~ m/\s+(src|href)="(.+?)"/gi;
+        my @matches3 = $content =~ m/<meta\s+http\-equiv=.*?refresh(.*)URL=([^'"]+)/gi;
         my $x=0;
         for my $match (@matches1, @matches2, @matches3) {
             $x++;
             next if $x%2==1;
-            next if $match =~ m/^ssh/;
+            next if $match =~ m/^irc:/;
+            next if $match =~ m/^ssh:/;
             next if $match =~ m/^mailto:/;
             next if $match =~ m/^javascript:/;
             next if $match =~ m/\/"\+/;
@@ -235,12 +218,16 @@ sub test_page {
             if($match =~ m/^\#(.*)$/mx) {
                 $local_check_anchors->{$1} = 1;
             } else {
+                if($match =~ m#^//(home|var|opt|usr)/#mx) {
+                    fail("wrong link: ".$match." on page: ".$opts->{'url'});
+                    next;
+                }
                 $match = _relative_url($match, $request->base()->as_string());
-                if(($match =~ m/^https?:/ || $match =~ m/^\/\//) && $match !~ m/^\Q$mainurl\E/) {
+                if(($match =~ m/^https?:/ || $match =~ m|^//|mx) && $match !~ m/^\Q$mainurl\E/) {
                     next unless $match =~ $opts->{'check_external_links'};
                     $links_to_check->{$match} = 0 unless defined $links_to_check->{$match};
                 }
-                elsif($match =~ m/^\Q$mainurl\E/mx and $match !~ m/\.(css|js|gif|jpg|png)$/mx) {
+                elsif($match =~ m/^\Q$mainurl\E/mx and $match !~ m/\.(css|js|gif|jpg|png|ico)$/mxi) {
                     $pages_to_check->{$match} = 0 unless defined $pages_to_check->{$match};
                     $link_referer->{$match}->{$opts->{'url'}} = 1;
                 } else {
@@ -297,9 +284,15 @@ sub remove_some_exceptions_from_lint_errors {
         my $err_str = $error->as_string;
         next if $err_str =~ m/\QUnknown attribute "role" for tag\E/imx;
         next if $err_str =~ m/\QUnknown element <footer>\E/imx;
+        next if $err_str =~ m/\QUnknown element <header>\E/imx;
         next if $err_str =~ m/\QUnknown element <nav>\E/imx;
+        next if $err_str =~ m/\Qshould be written as\E/imx;
         next if $err_str =~ m/\QUnknown attribute "data-\E/imx;
         next if $err_str =~ m/\QUnknown attribute "charset" for tag <meta>\E/imx;
+        next if $err_str =~ m/\QUnknown attribute "aria-hidden" for tag\E/imx;
+        next if $err_str =~ m/\QUnknown attribute "aria-label" for tag\E/imx;
+        next if $err_str =~ m/\QUnknown attribute "name" for tag <div>\E/imx;
+        next if $err_str =~ m/\QUnknown attribute "name" for tag <hr>\E/imx;
         next if $err_str =~ m/<img[^>]+?\Q> tag has no HEIGHT and WIDTH attributes\E/imx;
         next if $err_str =~ m/<img[^>]+?\Q> does not have ALT text defined\E/imx;
         push @return, $error->as_string;
@@ -325,21 +318,41 @@ sub _relative_url {
     my $newloc = $url;
     # absolute url
     if($location =~ m/^\//mx) {
-        $newloc = $url;
-        $newloc =~ s|^(.*?)://(.*?)/.*$|$1://$2|gmx;
-        $newloc .= $location;
-        return $newloc;
+        if($url =~ m/^file:/) {
+            $newloc = "file://".Cwd::getcwd().$location;
+            return $newloc;
+        } else {
+            $newloc = $url;
+            $newloc =~ s|^(.*?)://(.*?)/.*$|$1://$2|gmx;
+            $newloc .= $location;
+            return $newloc;
+        }
     }
     # relative url
     $newloc    =~ s/[^\/]+$//gmx;
     $newloc    .= $location;
     while($newloc =~ s|/[^\/]+/\.\./|/|gmx) {}
+    $newloc =~ s|/\./|/|gmx;
     return $newloc;
 }
 
 #########################
 sub _request {
     my($url, $method) = @_;
+    if($url =~ m|^file://(.*)$|mx) {
+        # fake index file instead of using directory index all the time
+        my $file = $1;
+        $file =~ s|/$||gmx;
+        if(-d $file) {
+            if(-e $file.'/index.html') {
+                $file = $file.'/index.html';
+            } else {
+                my @files = glob($file.'/index.*');
+                $file = $files[0];
+            }
+            $url = 'file://'.$file;
+        }
+    }
     $method = "GET" unless defined $method;
     my $ua = LWP::UserAgent->new;
     my $request;
@@ -383,6 +396,8 @@ sub bail_out_req {
 #########################
 sub _check_anchor {
     my($anchor, $url, $content, $samepage) = @_;
+    return if(!defined $anchor);
+    return if(defined $anchor and $anchor eq '');
     my $anchors  = {};
     my @matches1 = $content =~ m/(<.*?(name|id)=['"]{1}(\Q$anchor\E)['"]{1}.*?>)/;
     $anchors->{$anchor} = 1 if scalar @matches1 > 0;
